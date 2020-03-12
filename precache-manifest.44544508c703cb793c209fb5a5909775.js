a70dfb5b5d24ddc3567ed6acd34c3bf084a5a3bf
김도영 <ehdud40@ktnet.co.kr>
2020-03-12 09:38:41 +0900

    Updates

---
+++ precache-manifest.44544508c703cb793c209fb5a5909775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c9c6674ab57da1fdfc5f36df9de4a2",
+    "url": "/moive_app_2020/index.html"
+  },
+  {
+    "revision": "7e0626ffa9efd7006660",
+    "url": "/moive_app_2020/static/css/main.cb582e3c.chunk.css"
+  },
+  {
+    "revision": "ee5f40fffa95eb75bff2",
+    "url": "/moive_app_2020/static/js/2.41708d2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/moive_app_2020/static/js/2.41708d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0626ffa9efd7006660",
+    "url": "/moive_app_2020/static/js/main.0a128db3.chunk.js"
+  },
+  {
+    "revision": "975ad204aa1198d4f1ae",
+    "url": "/moive_app_2020/static/js/runtime-main.2dbf57ba.js"
+  }
+]);
